cbb4613cb68856d8da5ada1ab6df235d4a52cba1
dezo07 <blaze.desautels@gmail.com>
2023-05-11 19:03:19 -0400

    asw

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,30 @@ import './App.css';
 function App() {
   return (
     <>
-    
+      <section className='flex flex-col h-[100vh] bg-red-100 '>
+
+        {/* Partie Drap*/}
+
+        <div className='bg-blue-100 flex-[1]'></div>
+
+
+
+
+        {/* Partie Dezo*/}
+        <div className='flex gap-3 bg-yellow-100 flex-[10]'>
+
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+          <div className='h-[350px] w-[300px] bg-green-100'></div>
+        </div>
+      </section>
     </>
   );
 }
